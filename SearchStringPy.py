1bf2874411e0c7413452c4892c34c8dd88715794
LeahBasson <leahbasson54@gmail.com>
2025-07-15 23:11:48 +0200

    :rocket:

--- SearchStringPy.py
+++ SearchStringPy.py
@@ -1,19 +1,14 @@
 # a) Assign any string value to a variable. Test if the string contains the letter K.
 #Print an appropriate message. The string contains a K or The string does not contain a K.
 
-name = "Kite"
+name = "Leah"
 
 for index in range(len(name)):
-    print(index, name[index])
+    print(name[index])
 
-if name[index] == "K":
+if "K" in name:
     print("The string contains a K.")
 else:
     print("The string does not contain a K.")
 
 
-# for characters in name:
-#     if character == name.index("K"):
-#         print ("The string contains a K")
-#     else:
-#         print("The string does not contain a K")
