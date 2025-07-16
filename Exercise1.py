5b51e9fe7877e8b6c12719dcdf7ddd92766df769
Leah Basson <230146740@mycput.ac.za>
2025-07-16 12:56:10 +0200

    :zap:

---
+++ Exercise1.py
@@ -0,0 +1,9 @@
+#Asks the user for their name.
+#Prints the first and last character of it.
+#Then,modify it to also print the second and second-to-last characters.
+
+name = input("Enter name: ")
+print("First letter: ",name[0::-1])
+print("Last letter: ",name[-1])
+print ("Second letter: ",name[1])
+print ("Second last letter: ",name[-2])
