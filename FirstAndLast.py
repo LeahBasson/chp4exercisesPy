1ebcf3df75cd1893eb6791f1103a0e7345edead9
Leah Basson <230146740@mycput.ac.za>
2025-07-23 11:45:28 +0200

    :fire:

--- FirstAndLast.py
+++ FirstAndLast.py
@@ -3,7 +3,7 @@
 #Then,modify it to also print the second and second-to-last characters.
 
 name = input("Enter your name: ")
-print("First letter: ",name[0::-1])
+print("First letter: ",name[0])
 print("Last letter: ",name[-1])
 print ("Second letter: ",name[1])
 print ("Second last letter: ",name[-2])
